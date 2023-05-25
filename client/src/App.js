41560801bf121e133f4354318aac6aee9bad4dc7
ilkoTheTiger <ilkothetiger@gmail.com>
2023-05-26 00:01:28 +0300

    Implemented functionality for finding unique pairs by project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Papa from 'papaparse';
-import { useState } from 'react';
-import { formatDate } from './utils/formatDateUtils';
+import { useState, useEffect } from 'react';
+import { formatDate, dateDifferenceInDays } from './utils/formatDateUtils';
 import { Row } from './components/Row/Row';
 
 import {
@@ -17,6 +17,7 @@ import {
 function App() {
 
   const [data, setData] = useState([]);
+  const [results, setResults] = useState([]);
 
   const onSubmitHandler = (e) => {
     e.preventDefault()
@@ -31,7 +32,7 @@ function App() {
       try {
         return formatDate(new Date(val))
       } catch {
-        console.log(`Not a Date ${val}`)
+        console.log(`Not a recognized Date Format: ${val}`)
       }
     }
     return val;
@@ -44,13 +45,127 @@ function App() {
       transform: transform,
       skipEmptyLines: true,
       complete: function (results) {
-        console.log(results.data)
         setData(results.data)
       },
     });
   };
 
-  console.log(data)
+  const setOfProjects = (data) => {
+    const projects = new Set()
+    data?.map(row => projects.add(row.ProjectID))
+    return projects
+  }
+
+  const projects = setOfProjects(data)
+
+  const makeProjectsObject = () => {
+    let projectsObject = Object.fromEntries(projects.entries())
+    for (const projectID of Object.values(projectsObject)) {
+      // projectsObject = { ...projectsObject, '12':{empId: 1, emp2Id: 2, daysWorked: 3}}
+
+      let employeesByProjectArray = data?.filter(row => row.ProjectID == projectID)
+      let n = employeesByProjectArray.length;
+      const pairs = getPairsByProject(employeesByProjectArray, n)
+      // getPairByProject(employeesByProjectArray)
+      // projectsObject[projectID] = [data[0]]
+      for (const pair in pairs) {
+        let first = data?.filter(row => row.EmpID == pairs[pair][0] && projectID == row.ProjectID)
+        let second = data?.filter(row => row.EmpID == pairs[pair][1] && projectID == row.ProjectID)
+        const daysWorkedTogether = findDaysWorkedTogether(first[0].DateFrom, second[0].DateFrom, first[0].DateTo, second[0].DateTo)
+
+        if (projectsObject[projectID].DaysWorked < daysWorkedTogether || !projectsObject[projectID].DaysWorked) {
+          projectsObject[projectID] = {
+            id: 1,
+            FirstEmpID: pairs[pair][0],
+            SecondEmpID: pairs[pair][1],
+            ProjectID: projectID,
+            DaysWorked: daysWorkedTogether
+          }
+          setResults(projectsObject[projectID])
+        }
+      }
+    }
+  }
+
+  const findDaysWorkedTogether = (firstDateFrom, secondDateFrom, firstDateTo, secondDateTo) => {
+    const range1Start = new Date(firstDateFrom);
+    const range1End = new Date(firstDateTo);
+
+    const range2Start = new Date(secondDateFrom);
+    const range2End = new Date(secondDateTo);
+
+    if (range1Start < range2End && range2Start < range1End) {
+      let daysWorked = dateDifferenceInDays(Math.max(range1Start, range2Start), Math.min(range1End, range2End))
+      return daysWorked
+    }
+    return 0
+  }
+
+  function hasPair(array, a, b) {
+    let pairStatus = false;
+    for (let pair of array) {
+      console.log(pair)
+      if (pair.includes(a) && pair.includes(b)) {
+        pairStatus = true;
+      }
+    }
+    return pairStatus;
+  }
+
+  const getPairsByProject = (arr, n) => {
+    let pairs = []
+    for (let i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        if (i != j && !hasPair(pairs, arr[i].EmpID, arr[j].EmpID)) {
+          pairs.push([arr[i].EmpID, arr[j].EmpID])
+        }
+      }
+    }
+
+    if (pairs.length < 1) {
+      return;
+    }
+
+    return pairs
+  }
+
+  const getBestPairByProject = (array) => {
+    // console.log(array)
+
+    // return {
+    //   firstEmp.EmpID,
+    //   secondEmp.EmpID,
+    //   firstEmp.projectID,
+    //   daysWorked: 0
+    // }
+  }
+
+  // const populateProjects = () => {
+  //   let projects = {};
+  //   for (const entry of setOfProjects.values()) {
+  //     projects = {...projects, entry}
+  //   }
+  //   console.log(projects)
+  // }
+
+  const findPair = (project) => {
+    // const result = {
+    //   id: true,
+    //   FirstEmpID: true,
+    //   SecondEmpID: true,
+    //   ProjectID: true,
+    //   DaysWorked: true,
+    // }
+
+    makeProjectsObject()
+
+
+    // return result;
+  }
+
+  useEffect(() => {
+    setResults(findPair());
+  }, [data]);
 
   return (
     <>
@@ -78,10 +193,10 @@ function App() {
               </Tr>
             </Thead>
             <Tbody>
-            {data?.map((data) => (
-              <Row
-                key={data.id}
-                {...data} />))}
+              {data?.map((data) => (
+                <Row
+                  key={data.id}
+                  {...data} />))}
 
             </Tbody>
             {/* <Tfoot>
