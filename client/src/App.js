be5213f949a179af9dbb2db8e108542f1e050d57
ilkoTheTiger <ilkothetiger@gmail.com>
2023-05-25 14:46:52 +0300

    File received from input

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
 
 function App() {
+
+  // const NULL = new Date()
+  // console.log(NULL.toISOString().split('T')[0])
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(e.files)
+  }
+
+  const changeHandler = (event) => {
+    console.log(event.target.files[0])
+  };
+
   return (
     <>
-
+      <div>
+        {/* File Uploader */}
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="file"
+            name="file"
+            accept=".csv"
+            onChange={changeHandler}
+            style={{ display: "block", margin: "10px auto" }}
+          />
+          <button>Send</button>
+        </form>
+      </div>
     </>
   );
 }
