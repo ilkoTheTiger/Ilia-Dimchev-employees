27869d21d55e102952a091d07ca208f581f6bc38
ilkoTheTiger <ilkothetiger@gmail.com>
2023-05-25 16:48:16 +0300

    Functionality added to formatDateUtils.js

--- client/src/utils/formatDateUtils.js
+++ client/src/utils/formatDateUtils.js
@@ -4,3 +4,7 @@ export const formatDate = (date) => {
         ${String(date.getMonth() + 1).padStart(2, '0')}-
         ${String(date.getDate()).padStart(2, '0')}`.replace(/\s+/g, '').trim();
 };
+
+export const dateDifferenceInDays = (startDate, endDate) => {
+    return (new Date(endDate) - new Date(startDate)) / 86400000
+}
