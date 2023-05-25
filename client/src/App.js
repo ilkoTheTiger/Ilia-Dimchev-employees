f4688240c512a39aecf88ee2b74ac82f88b2b771
ilkoTheTiger <ilkothetiger@gmail.com>
2023-05-25 16:05:28 +0300

    Multi-date formats supported

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,62 @@
 import './App.css';
 import Papa from 'papaparse';
+import { useState } from 'react';
+import { formatDate } from './utils/formatDateUtils';
 
 function App() {
 
-  // const NULL = new Date()
-  // console.log(NULL.toISOString().split('T')[0])
+  const [data, setData] = useState([]);
 
   const onSubmitHandler = (e) => {
     e.preventDefault()
-    console.log(e.files)
+    console.log("show")
   }
 
-const transform = (val) => {
+  const transform = (val, column) => {
     if (val == "NULL") {
       val = new Date().toISOString().split('T')[0]
+    }
+    if (column == "DateTo" || column == "DateFrom") {
+      try {
+        return formatDate(new Date(val))
+      } catch {
+        console.log(`Not a Date ${val}`)
+      }
     }
     return val;
   }
 
-const changeHandler = (event) => {
-  Papa.parse(event.target.files[0], {
-    header: true,
-    delimiter: ", ",
-    transform: transform,
-    skipEmptyLines: true,
-    complete: function (results) {
-      console.log(results.data)
-    },
-  });
-};
+  const changeHandler = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      delimiter: ", ",
+      transform: transform,
+      skipEmptyLines: true,
+      complete: function (results) {
+        console.log(results.data)
+        setData(results.data)
+      },
+    });
+  };
 
-return (
-  <>
-    <div>
-      {/* File Uploader */}
-      <form onSubmit={onSubmitHandler}>
-        <input
-          type="file"
-          name="file"
-          accept=".csv"
-          onChange={changeHandler}
-          style={{ display: "block", margin: "10px auto" }}
-        />
-        <button>Send</button>
-      </form>
-    </div>
-  </>
-);
+  console.log(data)
+
+  return (
+    <>
+      <div>
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="file"
+            name="file"
+            accept=".csv"
+            onChange={changeHandler}
+            style={{ display: "block", margin: "10px auto" }}
+          />
+          <button>Show Data</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/utils/formatDateUtils.js
@@ -0,0 +1,6 @@
+export const formatDate = (date) => {
+    return `
+        ${date.getFullYear()}-
+        ${String(date.getMonth() + 1).padStart(2, '0')}-
+        ${String(date.getDate()).padStart(2, '0')}`.replace(/\s+/g, '').trim();
+};
